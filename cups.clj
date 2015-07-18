fac2d70c9fd9ac32fd3da35cf2723ff7eadec41d
fyquah95 <quah.fy95@gmail.com>
2015-07-18 13:53:49 +0100

    Refactor clojure solution

--- cups.clj
+++ cups.clj
@@ -1,88 +1,40 @@
-(if (>= (count *command-line-args*) 4)
-  (do 
-    (def max-x (Integer. (nth *command-line-args* 1)))
-    (def max-y (Integer. (nth *command-line-args* 2)))
-    (def target-volume (Integer. (nth *command-line-args* 3)))
-  )
-  (do
-    (def max-x 9)
-    (def max-y 4)
-    (def target-volume 2)
-  )
-)
+(defn make-get-next-states [max-x max-y]
+  (fn [state]
+    (let [x (:x state) y (:y state)]
+      (set [{:x (- x (- (min max-y (+ y x)) y)) :y (min max-y (+ y x))}
+            {:x (min max-x (+ x y)) :y (- y (- (min max-x (+ x y)) x))}
+            {:x x :y max-y}
+            {:x max-x :y y}
+            {:x 0 :y y}
+            {:x x :y 0}]))))
 
-(let [max-x max-x max-y max-y target-volume target-volume]
-  (defstruct cup-state :x :y)
+(defn transverse [root-state get-next-states is-target-volume?]
+  (->> (iterate (fn [[arr visited]]
+                  (let [next-states (filter #(not (contains? visited %))
+                                            (-> arr first last get-next-states))]
+                    [(concat (next arr)
+                            (map #(conj (first arr) %) next-states))
+                     (clojure.set/union visited (set next-states))]))
+           [[[root-state]] #{}] )
+    (map first)
+    (filter #(or (empty? %) (is-target-volume? (-> % last last))))
+    first last))
 
-  (defn is-target-volume? [state]
-    (or 
-      (= target-volume (get state :x))
-      (= target-volume (get state :x))
-    )
-  )
+(defn make-is-target-volume? [target-volume]
+  (fn [state] (or (= target-volume (:x state)) (= target-volume (:y state)))))
 
-  (defn get-next-states [state]
-    (def x (get state :x))
-    (def y (get state :y))
-    [
-      ; pour x -> y
-      (struct cup-state (- x (- (min max-y (+ y x)) y)) (min max-y (+ y x)) )
-      ; pour y -> x
-      (struct cup-state (min max-x (+ y x)) (- y (- (min max-x (+ y x)) x)) )
-      ; fill x
-      (struct cup-state max-x y)
-      ; fill y
-      (struct cup-state x max-y)
-      ; empty x
-      (struct cup-state 0 y)
-      ; empty y
-      (struct cup-state x 0)
-    ]
-  )
+(defn solve [max-x max-y target-volume]
+  (transverse {:x 0 :y 0}
+              (make-get-next-states max-x max-y)
+              (make-is-target-volume? target-volume)))
 
-  (defn transverse [stack visited]
-    (def latest-state (last (first stack)))
-
-    (let [ [accepted new-visited]
-      (loop [ states (get-next-states latest-state) v visited arr []]
-        (if (empty? states)
-          [arr v]
-          (do
-            (def state (first states))
-            (if (contains? v state)
-              (recur (next states) v arr)
-              (recur (next states) (conj v state) (conj arr state))
-            )              
-          )
-        )
-      )]
-
-      [ (concat (next stack) (map #(conj (first stack) %) accepted)) new-visited ]        
-    )
-  )
-
-  (def root-state (struct cup-state 0 0) )
-  (def final (loop [stack (list [ root-state ] ) visited #{ root-state } ]
-    (if (empty? stack) 
-      false
-      (do 
-        (if (is-target-volume? (last (first stack) ))
-          (first stack)
-          (let [ [stack visited] (transverse stack visited) ]
-            (recur stack visited)
-          )
-        )
-      )
-    )
-  ))
-
-  (if-not final
-    (println "No possible sequence found!")
-    (do
-      (println "This will take" (count final) "steps!")
-      (doseq [ entry final ]
-        (println (str "(" (entry :x) ", " (entry :y) ")"))
-      )
-    )
-  )
-)
+; NB: unpure methods (interacts with the outside world) follows:
+(let [flag? (>= (count *command-line-args*) 4)
+      max-x (if flag? (Integer. (nth *command-line-args* 1)) 9)
+      max-y (if flag? (Integer. (nth *command-line-args* 2)) 4)
+      target (if flag? (Integer. (nth *command-line-args* 3)) 2)
+      soln (solve max-x max-y target)]
+  (if soln
+    (do (println "Found a solution in" (- (count soln) 1) "steps!")
+      (doseq [step soln] (println step)))
+    (println "Solution not found!")))
